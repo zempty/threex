1254d17237e0774368468d056e182152ec8251ed
zempty <zempty@163.com>
2017-03-06 17:27:32 +0800

    .

---
+++ src/main/java/wang/threex/test/MainT.java
@@ -0,0 +1,12 @@
+package wang.threex.test;
+
+import wang.threex.test.xml.XmlDemo;
+
+public class MainT {
+
+    public static void main(String[] args) {
+        XmlDemo xml = new XmlDemo();
+        xml.createXml("我爱你啊");
+    }
+
+}
