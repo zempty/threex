2a1284fa6bd48a497357d60af9f77e9f9b953d53
zempty <zempty@163.com>
2017-03-15 11:31:16 +0800

    .

--- src/main/java/com/zhu2chu/test/MainT.java
+++ src/main/java/com/zhu2chu/test/MainT.java
@@ -3,12 +3,6 @@ package com.zhu2chu.test;
 public class MainT {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        BigDecimal a = new BigDecimal("23.689687265498354642165");
-        a = a.setScale(2, BigDecimal.ROUND_DOWN);
-        System.out.println(a);
-=======
->>>>>>> remotes/origin/master
     }
 
 }
