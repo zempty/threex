3985cec781a6fedbba7273318fc24fc3526e417f
zempty <zempty@163.com>
2017-03-15 09:56:30 +0800

    ...

--- src/main/java/com/zhu2chu/test/MainT.java
+++ src/main/java/com/zhu2chu/test/MainT.java
@@ -5,10 +5,8 @@ import java.math.BigDecimal;
 public class MainT {
 
     public static void main(String[] args) {
-        BigDecimal s = new BigDecimal(0.06+0.01);
-        int ss = 010;
-        System.out.println(s.doubleValue());
-        System.out.println(ss);
+        BigDecimal a = new BigDecimal(Double.toString(2.2));
+        System.out.println(123.3f/100.0f);
     }
 
 }
