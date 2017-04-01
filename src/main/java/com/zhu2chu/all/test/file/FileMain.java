3a50eb5776877ef77ba49a425c4980bc5c4c0a32
zempty <zempty@163.com>
2017-04-01 10:26:31 +0800

    .

--- src/main/java/com/zhu2chu/all/test/file/FileMain.java
+++ src/main/java/com/zhu2chu/all/test/file/FileMain.java
@@ -2,10 +2,12 @@ package com.zhu2chu.all.test.file;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.commons.codec.binary.Base64;
 
 import com.jfinal.log.Log;
 
@@ -53,13 +55,16 @@ public class FileMain {
     public static void readFileByBytes(String filename) {
         File f = new File(filename);
         InputStream in = null;
+        OutputStream out = null;
         System.out.println("以字节为单位读取文件内容，一次读一个字节：");
         try {
             in = new FileInputStream(f);
             int tempByte;
-            while ((tempByte=in.read()) != -1) {
+            /*while ((tempByte=in.read()) != -1) {
                 System.out.write(tempByte);
-            }
+            }*/
+            byte[] s = Base64.decodeBase64("5oiR5Lus5LiA5LiL5Zyo6L+Z6YeM5ZWK");
+            System.out.println(new String(s, "UTF-8"));
         } catch (IOException e) {
             e.printStackTrace();
         }
