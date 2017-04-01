83c9798948bff897d55125b7b9898e7dabf90b1b
zempty <zempty@163.com>
2017-04-01 10:11:47 +0800

    .

--- src/main/java/com/zhu2chu/all/test/MainT.java
+++ src/main/java/com/zhu2chu/all/test/MainT.java
@@ -1,8 +1,14 @@
 package com.zhu2chu.all.test;
 
+import com.jfinal.plugin.activerecord.Record;
+
 public class MainT {
 
     public static void main(String[] args) {
+        Record s = new Record();
+        Object g = (Object)s;
+
+        System.out.println(g);
     }
 
 }
