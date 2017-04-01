9674d050a67fd98cc438fd85608d85c9842fdbcd
zempty <zempty@163.com>
2017-04-01 10:13:54 +0800

    .

--- src/main/java/com/zhu2chu/all/test/MainT.java
+++ src/main/java/com/zhu2chu/all/test/MainT.java
@@ -1,24 +1,20 @@
 package com.zhu2chu.all.test;
 
-<<<<<<< HEAD
 import com.jfinal.plugin.activerecord.Record;
-=======
 import java.io.IOException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
->>>>>>> remotes/origin/master
 
 public class MainT {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
         Record s = new Record();
         Object g = (Object)s;
 
         System.out.println(g);
-=======
+
         try {
             Document doc = Jsoup.connect("https://www.baidu.com").get();
             Element e = doc.body();
@@ -26,8 +22,7 @@ public class MainT {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
->>>>>>> remotes/origin/master
+
     }
 
 }
